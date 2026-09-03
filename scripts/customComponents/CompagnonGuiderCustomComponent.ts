c31985c9f83835f6a1f6592a6342d9ec2e24f940
faouz-dev <faouzanekouko8@gmail.com>
2026-09-03 22:19:36 +0000

    Mise à jour de la recette cpn_guider : changement des ingrédients et ajout de la logique de vérification des compagnons dans le composant CompagnonGuider. Ajout de fichiers de langue pour le support multilingue.

--- scripts/customComponents/CompagnonGuiderCustomComponent.ts
+++ scripts/customComponents/CompagnonGuiderCustomComponent.ts
@@ -5,20 +5,32 @@ import type {
   ItemComponentUseEvent,
   ItemComponentUseOnEvent,
   ItemCustomComponent,
+  Player,
 } from "@minecraft/server";
 import { COMPAGNON_TYPE } from "../constants/compagnonType";
 import { Vector3Utils } from "@minecraft/math";
+import { COMPAGNONS } from "../constants";
+import { debugLog } from "../functions";
 
 export class CompagnonGuiderCustomComponent implements ItemCustomComponent {
   constructor() {
     this.onUse = this.onUse.bind(this);
   }
 
+  getCompagnonsOrPreventIfDontHave(player: Player) {
+    const haveCompagnon = COMPAGNONS.get(player.id);
+    if (!haveCompagnon) {
+      player.sendMessage({ translate: "message.cpn_guider.dont_have_compagnon" });
+      return false;
+    }
+    return haveCompagnon;
+  }
+
   onUse(event: ItemComponentUseEvent, param: CustomComponentParameters) {
     const { source } = event;
 
     const useOnEntity = source.getEntitiesFromViewDirection({
-      type: COMPAGNON_TYPE,
+      tags: [COMPAGNON_TYPE],
       maxDistance: 5,
     });
 
@@ -28,7 +40,7 @@ export class CompagnonGuiderCustomComponent implements ItemCustomComponent {
     const entityDistance = useOnEntity.length > 0 ? useOnEntity[0].distance : undefined;
     const blockDistance = useOnBlock ? Vector3Utils.distance(useOnBlock.block.location, source.location) : undefined;
 
-    if ((entityDistance && blockDistance && entityDistance < blockDistance) || !blockDistance) {
+    if (entityDistance && ((blockDistance && entityDistance < blockDistance) || !blockDistance)) {
       usedOn = "entity";
     } else if (blockDistance) {
       usedOn = "block";
@@ -36,14 +48,27 @@ export class CompagnonGuiderCustomComponent implements ItemCustomComponent {
 
     switch (usedOn) {
       case "entity":
+        const entity = useOnEntity[0].entity;
         // TODO: Use on entity
-        source.sendMessage("Use on entity");
+        debugLog("[CompagnonGuiderCustomComponent] Using on an entity : " + entity.id);
+        const haveCompagnon = this.getCompagnonsOrPreventIfDontHave(source);
+        if (haveCompagnon) {
+          const compagnon = haveCompagnon;
+          const isHisCompagnon = entity.id === compagnon.compagnon.id;
+          if (!isHisCompagnon) {
+            debugLog("[CompagnonGuiderCustomComponent] Not his compagnon");
+            return source.sendMessage({ translate: "message.cpm_guider.only_use_on_own_compagnon" });
+          }
+          debugLog("[CompagnonGuiderCustomComponent] Using on his compagnon");
+        }
         break;
       case "block":
         // TODO: Use on block
-        source.sendMessage("Use on block");
+        const block = useOnBlock!.block;
+        debugLog("[CompagnonGuiderCustomComponent] Using on a block : " + block.typeId);
         break;
       default:
+        debugLog("[CompagnonGuiderCustomComponent] No target");
         break;
     }
   }
